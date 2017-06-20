fe52c74fd65c80a2ec9c8073f80a41a4cef5edd4
Bastian Allgeier <mail@bastianallgeier.com>
2017-06-20 17:44:22 +0200

    Add headline sizes

--- components/Text/Headline/Headline.vue
+++ components/Text/Headline/Headline.vue
@@ -1,14 +1,28 @@
 <template>
-  <h2 class="kirby-headline"><slot /></h2>
+  <h2 class="kirby-headline" :data-size="size"><slot /></h2>
 </template>
+
+<script>
+
+export default {
+  props: ['size']
+}
+
+</script>
 
 <style lang="scss">
 
 .kirby-headline {
-  font-size: 1rem;
+  font-size: $font-size-medium;
   font-weight: 500;
   margin-bottom: .5rem;
   line-height: 1.5em;
+}
+.kirby-headline[data-size="small"] {
+  font-size: $font-size-small;
+}
+.kirby-headline[data-size="large"] {
+  font-size: $font-size-large;
 }
 
 </style>
