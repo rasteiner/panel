4cf4410de738c646589dcbfacbfda8f187b15b51
Nico Hoffmann <on-github@nhoffmann.com>
2017-09-01 17:47:23 +0200

    Split Fielld props mixins

--- src/Ui/Forms/Fields/TextField/TextField.props.js
+++ src/Ui/Forms/Fields/TextField/TextField.props.js
@@ -1,16 +1,9 @@
+
+import Props from '../Field.props.js';
+
 export default {
+  mixins: [Props],
   props: {
-    'label': {
-      type: String,
-      default: ''
-    }, 
-    'icon': {
-      default: false
-    }, 
-    'name': {
-      type: String,
-      default: ''
-    },
     'value': {
       type: String,
       default: ''
@@ -19,29 +12,9 @@ export default {
       type: String,
       default: ''
     },
-    'required': {
-      type: Boolean,
-      default: false
-    }, 
-    'readonly': {
-      type: Boolean,
-      default: false
-    },
-    'autofocus': {
-      type: Boolean,
-      default: false
-    },
     'autocomplete': {
       type: [String, Boolean],
       default: 'off'
-    },
-    'help': {
-      type: String,
-      default: ''
-    },
-    'type': {
-      type: String,
-      default: 'text'
     }
-  }    
+  }
 }
