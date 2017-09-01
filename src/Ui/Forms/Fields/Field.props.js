392a592d2655a0471cb64738ae4a0834bd277986
Nico Hoffmann <on-github@nhoffmann.com>
2017-09-01 17:47:23 +0200

    Split Fielld props mixins

---
+++ src/Ui/Forms/Fields/Field.props.js
@@ -0,0 +1,39 @@
+export default {
+  props: {
+    'label': {
+      type: String,
+      default: ''
+    },
+    'icon': {
+      default: false
+    },
+    'name': {
+      type: String,
+      default: ''
+    },
+    'value': {
+      type: String,
+      default: ''
+    },
+    'required': {
+      type: Boolean,
+      default: false
+    },
+    'readonly': {
+      type: Boolean,
+      default: false
+    },
+    'autofocus': {
+      type: Boolean,
+      default: false
+    },
+    'help': {
+      type: String,
+      default: ''
+    },
+    'type': {
+      type: String,
+      default: 'text'
+    }
+  }
+}
