0b3d27e1e4510cc3bc2dffd8d48fab087e55cc5c
Nico Hoffmann <on-github@nhoffmann.com>
2017-10-13 15:09:42 +0200

    Add label to info field

--- src/Ui/Forms/Fields/InfoField/InfoField.vue
+++ src/Ui/Forms/Fields/InfoField/InfoField.vue
@@ -1,17 +1,26 @@
 <template>
-  <kirby-txt>
-    {{ text }}
-  </kirby-txt>
+  <div class="kirby-info-field">
+    <kirby-headline>{{ label }}</kirby-headline>
+    <kirby-txt>
+      {{ text }}
+    </kirby-txt>
+  </div>
 </template>
 
 <script>
 
 export default {
   props: {
+    label: {
+      type: String,
+      default: 'Info'
+    },
     name: {
+      type: String,
       default: 'info'
     },
     type: {
+      type: String,
       default: 'info'
     },
     text: {
