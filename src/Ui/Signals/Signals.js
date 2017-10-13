c3aaeedb02da6ae776cb06d7917138aade6f1a59
Nico Hoffmann <on-github@nhoffmann.com>
2017-10-13 11:12:09 +0200

    Move Notification to Signals

--- src/Ui/Signals/Signals.js
+++ src/Ui/Signals/Signals.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Loader from './Loader/Loader.vue'
+import Notification from './Notification/Notification.vue';
 import Progress from './Progress/Progress.vue'
 
 Vue.component('kirby-loader', Loader);
+Vue.component('kirby-notification', Notification);
 Vue.component('kirby-progress', Progress);
