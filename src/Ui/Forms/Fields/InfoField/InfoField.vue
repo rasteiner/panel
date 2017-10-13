b936c1fca6bd2e5236520be018b1750c67db1724
Nico Hoffmann <on-github@nhoffmann.com>
2017-10-13 14:00:13 +0200

    Add InfoField
    Solves #47

---
+++ src/Ui/Forms/Fields/InfoField/InfoField.vue
@@ -0,0 +1,23 @@
+<template>
+  <kirby-txt>
+    {{ text }}
+  </kirby-txt>
+</template>
+
+<script>
+
+export default {
+  props: {
+    name: {
+      default: 'info'
+    },
+    type: {
+      default: 'info'
+    },
+    text: {
+      type: String
+    }
+  }
+}
+
+</script>
