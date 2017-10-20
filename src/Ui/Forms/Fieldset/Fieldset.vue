d762553e5f9566d943114a42c4319e39fa5f4e23
Nico Hoffmann <on-github@nhoffmann.com>
2017-10-20 15:16:23 +0200

    Placeholder for non existing field types
    Fix #166

--- src/Ui/Forms/Fieldset/Fieldset.vue
+++ src/Ui/Forms/Fieldset/Fieldset.vue
@@ -1,12 +1,16 @@
 <template>
   <kirby-grid class="kirby-fieldset" gutter="medium">
     <kirby-column v-for="(field, index) in fields" :key="index" :width="field.width || '1/1'">
-      <component :is="'kirby-' + field.type + '-field'" v-bind="field" :value="values[field.name]" @submit="$emit('submit')" @input="input(field.name, $event)" />
+      <component v-if="exists(field.type)" :is="'kirby-' + field.type + '-field'" v-bind="field" :value="values[field.name]" @submit="$emit('submit')" @input="input(field.name, $event)" />
+
+      <kirby-box v-else>Invalid field type: {{ field.type }}</kirby-box>
     </kirby-column>
   </kirby-grid>
 </template>
 
 <script>
+
+import Vue from 'vue';
 
 export default {
   props: {
@@ -32,6 +36,9 @@ export default {
     input (field, value) {
       this.data[field] = value;
       this.$emit('input', this.data);
+    },
+    exists (type) {
+      return Vue.options.components['kirby-' + type + '-field'];
     }
   }
 }
