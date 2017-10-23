ca18cd7a042ff4c30b77266dd49cea1dcd08bc19
Nico Hoffmann <on-github@nhoffmann.com>
2017-10-23 20:18:40 +0200

    Fix #199

--- src/Ui/Forms/Inputs/CheckboxInput/CheckboxInput.vue
+++ src/Ui/Forms/Inputs/CheckboxInput/CheckboxInput.vue
@@ -1,8 +1,8 @@
 <template>
 
   <div class="kirby-checkbox-input">
-    <input :id="name" type="checkbox" v-model="data" :checked="data" />
-    <label :for="name">
+    <input :id="name || _uid" type="checkbox" v-model="data" :checked="data" />
+    <label :for="name || _uid">
       <span class="checkbox" aria-hidden="true">
         <svg width="12" height="10" viewBox="0 0 12 10" xmlns="http://www.w3.org/2000/svg">
           <path d="M1 5l3.3 3L11 1" stroke-width="2" fill="none" fill-rule="evenodd" />
