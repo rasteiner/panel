1be0f4ad1440b52db42074434d3c14d3f36ba765
Bastian Allgeier <mail@bastianallgeier.com>
2017-10-28 00:30:54 +0200

    Visibility switching

--- src/App/Dialogs/Page/PageStatusDialog/PageStatusDialog.vue
+++ src/App/Dialogs/Page/PageStatusDialog/PageStatusDialog.vue
@@ -1,25 +1,6 @@
 <template>
-  <kirby-dialog ref="dialog" headline="Status" state="positive" button="Change" @submit="$refs.form.submit()">
-    <kirby-form ref="form" :fields="[
-      {
-        name: 'status',
-        label: 'Select a new status',
-        type: 'radio',
-        required: true,
-        options: [
-          {
-            value: 'unlisted',
-            text: 'Unlisted',
-            info: 'The page is only accessible via URL'
-          },
-          {
-            value: 'listed',
-            text: 'Public',
-            info: 'The page is public for anyone'
-          }
-        ]
-      }
-    ]" :values="$data" @submit="submit" />
+  <kirby-dialog ref="dialog" size="medium" state="positive" button="Change" @submit="$refs.form.submit()">
+    <kirby-form ref="form" :fields="fields" :values="$data" @submit="submit" />
   </kirby-dialog>
 </template>
 
@@ -32,22 +13,78 @@ export default {
   data () {
     return {
       page: {id: null},
-      status: null
+      status: null,
+      position: null
     };
+  },
+  computed: {
+    fields () {
+
+      let fields = [
+        {
+          name: 'status',
+          label: 'Select a new status',
+          type: 'radio',
+          required: true,
+          options: [
+            {
+              value: 'draft',
+              text: 'Draft',
+              info: 'The page is only accessible for logged in editors'
+            },
+            {
+              value: 'unlisted',
+              text: 'Unlisted',
+              info: 'The page is only accessible via URL'
+            },
+            {
+              value: 'listed',
+              text: 'Public',
+              info: 'The page is public for anyone'
+            }
+          ]
+        }
+      ];
+
+      if (this.status === 'listed') {
+        fields.push({
+          name: 'position',
+          label: 'Please select a position',
+          type: 'number',
+          help: 'This is definitely going to change :)',
+        });
+      }
+
+      return fields;
+
+    }
   },
   methods: {
     open (id) {
       this.$api.page.get(id).then((page) => {
-        this.page   = page;
-        this.status = page.isVisible ? 'listed' : 'unlisted';
+        this.page     = page;
+        this.status   = page.isVisible ? 'listed': 'unlisted';
+        this.position = page.num;
         this.$refs.dialog.open();
       }).catch((error) => {
         this.$store.dispatch('error', error.message);
       });
     },
     submit () {
-      this.$api.page.status(this.page.id, this.status, 1).then(() => {
-        this.$store.dispatch('success', 'The page is now ' + this.status);
+
+      if (this.status === 'draft') {
+        this.$store.dispatch('error', 'Drafts are not yet implemented');
+        return;
+      }
+
+      this.$api.page.status(this.page.id, this.status, this.position).then(() => {
+
+        if (this.status === 'listed') {
+          this.$store.dispatch('success', 'The page is now at position ' + this.position);
+        } else {
+          this.$store.dispatch('success', 'The page is now ' + this.status);
+        }
+
         this.$refs.dialog.close();
         this.$emit('success');
       }).catch((error) => {
