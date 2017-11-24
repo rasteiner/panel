58ddec6c94d3bdd5c47de9c0f8e506daff0bd329
Bastian Allgeier <mail@bastianallgeier.com>
2017-11-24 10:36:13 +0100

    Remove unneeded page and file collections

--- src/App/Collections/Collections.js
+++ src/App/Collections/Collections.js
@@ -1,9 +1,5 @@
 import Vue from 'vue'
 
-import Pages from './Pages/Pages.vue';
-import Files from './Files/Files.vue';
 import Users from './Users/Users.vue';
 
-Vue.component('kirby-pages-collection', Pages);
-Vue.component('kirby-files-collection', Files);
 Vue.component('kirby-users-collection', Users);
