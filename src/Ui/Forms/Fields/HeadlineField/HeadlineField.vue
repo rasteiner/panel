09267c43a8911a38057cfc59dc12fc07a46a31f5
Bastian Allgeier <mail@bastianallgeier.com>
2017-11-28 19:05:44 +0100

    Fix headline field style

--- src/Ui/Forms/Fields/HeadlineField/HeadlineField.vue
+++ src/Ui/Forms/Fields/HeadlineField/HeadlineField.vue
@@ -22,24 +22,27 @@ export default {
 
 <style lang="scss">
 
-  .kirby-headline-field {
-    position: relative;
-    margin-bottom: -.5rem;
-    padding-top: 3rem;
-    padding-left: 1.5em;
-    padding-bottom: .5rem;
-    border-bottom: 1px solid $color-border;
-    counter-increment: count;
-  }
+.kirby-headline-field {
+  position: relative;
+  margin-bottom: -.5rem;
+  padding-top: 3rem;
+  padding-left: 1.5em;
+  padding-bottom: .5rem;
+  border-bottom: 1px solid $color-border;
+  counter-increment: count;
+}
+.kirby-headline-field.kirby-headline > span {
+  padding: 0;
+}
 
-  .kirby-headline-field::before {
-    position:    absolute;
-    content:     counter(count, decimal-leading-zero);
-    bottom:      1px;
-    padding-bottom: .5rem;
-    left:        0;
-    color:       $color-focus;
-    font-family: $font-family-mono;
-    font-weight: 400;
-  }
+.kirby-headline-field::before {
+  position:    absolute;
+  content:     counter(count, decimal-leading-zero);
+  padding-bottom: .5rem;
+  left:        0;
+  color:       $color-focus;
+  font-family: $font-family-mono;
+  font-weight: 400;
+}
+
 </style>
