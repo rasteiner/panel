6849c6bbcb39639dc9d51dbff56caf3d58c7e72a
Nico Hoffmann <on-github@nhoffmann.com>
2018-02-17 18:42:10 +0100

    Fix DateFilter helper

--- src/App/Helpers/DateFilter.js
+++ src/App/Helpers/DateFilter.js
@@ -1,23 +1,20 @@
-import Vue from 'vue'
-import { DateTime } from 'luxon'
-import store from '../Store/Store.js';
+import Vue from "vue";
+import { DateTime } from "luxon";
+import store from "../Store/Store.js";
 
-
-Vue.filter('date', (value, output) => {
-
+Vue.filter("date", (value, output) => {
   if (!value) {
-    return value
+    return value;
   }
 
-  var dt
+  var dt;
   if (Array.isArray(value)) {
-    dt = DateTime.fromString(value[0], value[1])
+    dt = DateTime.fromString(value[0], value[1]);
   } else {
-    dt = DateTime.fromISO(value)
+    dt = DateTime.fromISO(value);
   }
 
-  return dt.
-          setLocale(store.state.language.locale).
-          toLocaleString(typeof output === 'string' ? DateTime[output] : output)
-
+  return dt
+    .setLocale(store.state.locale.id.replace("_", "-"))
+    .toLocaleString(typeof output === "string" ? DateTime[output] : output);
 });
