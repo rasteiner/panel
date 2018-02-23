9f16e1e067a13cd23a095ea7c1784143d766b230
Bastian Allgeier <mail@bastianallgeier.com>
2018-02-23 15:42:58 +0100

    Remove Fancy Input

--- src/Ui/Forms/Inputs/Inputs.js
+++ src/Ui/Forms/Inputs/Inputs.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 
 import CalendarInput from "./CalendarInput/CalendarInput.vue";
 import CheckboxInput from "./CheckboxInput/CheckboxInput.vue";
-import FancyInput from "./FancyInput/FancyInput.vue";
 import DateInput from "./DateInput/DateInput.vue";
 import RadioInput from "./RadioInput/RadioInput.vue";
 import SelectInput from "./SelectInput/SelectInput.vue";
@@ -11,7 +10,6 @@ import TimeInput from "./TimeInput/TimeInput.vue";
 Vue.component("kirby-calendar-input", CalendarInput);
 Vue.component("kirby-checkbox-input", CheckboxInput);
 Vue.component("kirby-date-input", DateInput);
-Vue.component("kirby-fancy-input", FancyInput);
 Vue.component("kirby-radio-input", RadioInput);
 Vue.component("kirby-select-input", SelectInput);
 Vue.component("kirby-time-input", TimeInput);
