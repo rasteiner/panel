3d1fa25a8fdc7a717ff59a782a86726806d90358
Bastian Allgeier <mail@bastianallgeier.com>
2018-02-23 15:48:24 +0100

    Remove broken user field for now

--- src/App/Components/Forms/Fields/Fields.js
+++ src/App/Components/Forms/Fields/Fields.js
@@ -1,7 +1,5 @@
 import Vue from "vue";
 
 import LanguageField from "./LanguageField/LanguageField.vue";
-import UserField from "./UserField/UserField.vue";
 
 Vue.component("kirby-language-field", LanguageField);
-Vue.component("kirby-user-field", UserField);
