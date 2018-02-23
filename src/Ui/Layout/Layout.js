60883fbd5a55fcceddcc4f3afa578edd731bdb43
Bastian Allgeier <mail@bastianallgeier.com>
2018-02-23 12:08:55 +0100

    Remove deprecated or broken components

--- src/Ui/Layout/Layout.js
+++ src/Ui/Layout/Layout.js
@@ -1,25 +1,21 @@
-import Vue from 'vue'
+import Vue from "vue";
 
-import Bar from './Bar/Bar.vue'
-import Box from './Box/Box.vue'
-import Card from './Card/Card.vue'
-import Cards from './Cards/Cards.vue'
-import Dropzone from './Dropzone/Dropzone.vue'
-import Grid from './Grid/Grid.vue'
-import Header from './Header/Header.vue'
-import Column from './Grid/Column.vue'
-import View from './View/View.vue'
-import Widget from './Widgets/Widget.vue'
-import Widgets from './Widgets/Widgets.vue'
+import Bar from "./Bar/Bar.vue";
+import Box from "./Box/Box.vue";
+import Card from "./Card/Card.vue";
+import Cards from "./Cards/Cards.vue";
+import Dropzone from "./Dropzone/Dropzone.vue";
+import Grid from "./Grid/Grid.vue";
+import Header from "./Header/Header.vue";
+import Column from "./Grid/Column.vue";
+import View from "./View/View.vue";
 
-Vue.component('kirby-bar', Bar);
-Vue.component('kirby-box', Box);
-Vue.component('kirby-card', Card);
-Vue.component('kirby-cards', Cards);
-Vue.component('kirby-dropzone', Dropzone);
-Vue.component('kirby-grid', Grid);
-Vue.component('kirby-header', Header);
-Vue.component('kirby-column', Column);
-Vue.component('kirby-view', View);
-Vue.component('kirby-widget', Widget);
-Vue.component('kirby-widgets', Widgets);
+Vue.component("kirby-bar", Bar);
+Vue.component("kirby-box", Box);
+Vue.component("kirby-card", Card);
+Vue.component("kirby-cards", Cards);
+Vue.component("kirby-dropzone", Dropzone);
+Vue.component("kirby-grid", Grid);
+Vue.component("kirby-header", Header);
+Vue.component("kirby-column", Column);
+Vue.component("kirby-view", View);
