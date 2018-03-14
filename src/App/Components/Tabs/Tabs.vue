abc58492f5773b7b840ff88876240c9c6e661dc6
Bastian Allgeier <mail@bastianallgeier.com>
2018-03-14 10:42:53 +0100

    First rough implementation of a publish bar

--- src/App/Components/Tabs/Tabs.vue
+++ src/App/Components/Tabs/Tabs.vue
@@ -1,5 +1,16 @@
 <template>
-  <kirby-sections :parent="parent" :columns="tab.columns" @submit="$emit('submit', $event)" />
+  <div>
+    <div class="kirby-publish-bar">
+      <p class="kirby-publish-bar-text">This page has unpublished changes!</p>
+      <nav class="kirby-publish-bar-buttons">
+        <kirby-button icon="preview">Preview</kirby-button>
+        <kirby-button icon="undo" state="negative">Discard</kirby-button>
+        <kirby-button icon="smile" state="positive">Publish</kirby-button>
+      </nav>
+    </div>
+
+    <kirby-sections :parent="parent" :columns="tab.columns" @submit="$emit('submit', $event)" />
+  </div>
 </template>
 
 <script>
@@ -24,3 +35,29 @@ export default {
   }
 };
 </script>
+
+<style lang="scss">
+.kirby-publish-bar {
+  display: flex;
+  align-items: center;
+  font-size: $font-size-small;
+  font-family: $font-family-mono;
+  border-radius: $border-radius;
+  margin-top: -1.5rem;
+  margin-bottom: 3rem;
+  padding: 0 1rem;
+  border-bottom: 1px solid $color-border;
+  border-left: 1px solid $color-border;
+  border-right: 1px solid $color-border;
+  background: $color-inset;
+}
+
+.kirby-publish-bar-text {
+  padding: 0.7rem 0;
+  margin-right: 1rem;
+  flex-grow: 1;
+}
+.kirby-publish-bar .kirby-button {
+  padding: 0.5rem;
+}
+</style>
