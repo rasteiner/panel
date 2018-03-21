502412179dedf05b500db46d43e0cc3cd03be528
Bastian Allgeier <mail@bastianallgeier.com>
2018-03-21 11:18:55 +0100

    Fix user blueprint advice

--- src/App/Views/UserView/UserView.vue
+++ src/App/Views/UserView/UserView.vue
@@ -49,7 +49,7 @@
       <kirby-tabs :key="'user-' + user.id + '-tabs'" v-if="user && tabs.length" :parent="'users/' + user.id" :tabs="tabs" ref="tabs" />
 
       <kirby-box v-else>
-        You can define additional sections and form fields for this user role in <strong>/site/blueprints/users/{{user.role}}.yml</strong>
+        You can define additional sections and form fields for this user role in <strong>/site/blueprints/users/{{user.role.name}}.yml</strong>
       </kirby-box>
 
       <kirby-user-role-dialog ref="role" @success="fetch" />
@@ -75,7 +75,9 @@ export default {
     return {
       tabs: [],
       user: {
-        role: null,
+        role: {
+          name: null
+        },
         name: null,
         language: null,
         prev: null,
