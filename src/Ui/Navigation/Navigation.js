5b695ba93f8000d209bcf870587df24ee9b93429
Bastian Allgeier <mail@bastianallgeier.com>
2018-03-26 21:24:10 +0200

    Remove old cruft

--- src/Ui/Navigation/Navigation.js
+++ src/Ui/Navigation/Navigation.js
@@ -1,19 +1,11 @@
-import Vue from 'vue'
+import Vue from "vue";
 
-import Breadcrumb from './Breadcrumb/Breadcrumb.vue'
-import BreadcrumbItem from './Breadcrumb/BreadcrumbItem.vue'
-import Dropdown from './Dropdown/Dropdown.vue'
-import DropdownContent from './Dropdown/DropdownContent.vue'
-import DropdownItem from './Dropdown/DropdownItem.vue'
-import Menu from './Menu/Menu.vue'
-import Pagination from './Pagination/Pagination.vue'
-import PrevNext from './PrevNext/PrevNext.vue'
+import Dropdown from "./Dropdown/Dropdown.vue";
+import DropdownContent from "./Dropdown/DropdownContent.vue";
+import DropdownItem from "./Dropdown/DropdownItem.vue";
+import Pagination from "./Pagination/Pagination.vue";
 
-Vue.component('kirby-breadcrumb', Breadcrumb);
-Vue.component('kirby-breadcrumb-item', BreadcrumbItem);
-Vue.component('kirby-dropdown', Dropdown);
-Vue.component('kirby-dropdown-content', DropdownContent);
-Vue.component('kirby-dropdown-item', DropdownItem);
-Vue.component('kirby-menu', Menu);
-Vue.component('kirby-pagination', Pagination);
-Vue.component('kirby-prev-next', PrevNext);
+Vue.component("kirby-dropdown", Dropdown);
+Vue.component("kirby-dropdown-content", DropdownContent);
+Vue.component("kirby-dropdown-item", DropdownItem);
+Vue.component("kirby-pagination", Pagination);
